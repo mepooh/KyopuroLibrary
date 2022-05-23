31ffb48aa79164297f53184b0c52978d9cd01ae6
mistralll <me.pooh441@gmail.com>
2022-05-23 18:22:05 +0900

    add: some files

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int main(){
+    
+}
